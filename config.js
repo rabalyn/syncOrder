8e735c3fc4332433347d00d2fab999f4e2d87ae5
@rabalyn <n.mundhenke@mailbox.org>
2018-06-14 22:04:32 +0200

    Reformat config

--- config.js
+++ config.js
@@ -1,14 +1,24 @@
 import path from 'path'
 
-let config = {}
+let config = {
+  app: {
+    domain: 'panf.de'
+  },
+  cookie: {
+    secret: 'foobar',
+    resave: false,
+    saveUninitialized: true
+  },
+  serialization: {
+    tableIdFilestorepath: path.join(__dirname, 'data', 'tableId.txt'),
+    ordersFilestorepath: path.join(__dirname, 'data', 'orders.json'),
+    metaFilestorepath: path.join(__dirname, 'data', 'meta.json'),
+    paiedFilestorepath: path.join(__dirname, 'data', 'paied.json'),
+    interval: 1000 * 60 * 5
+  },
+  socket: {
 
-config.serialization = {}
-config.serialization.tableIdFilestorepath = path.join(__dirname, 'data', 'tableId.txt')
-config.serialization.ordersFilestorepath = path.join(__dirname, 'data', 'orders.json')
-config.serialization.metaFilestorepath = path.join(__dirname, 'data', 'meta.json')
-config.serialization.paiedFilestorepath = path.join(__dirname, 'data', 'paied.json')
-config.serialization.interval = 1000 * 60 * 5
-
-config.socket = {}
+  }
+}
 
 module.exports = config
