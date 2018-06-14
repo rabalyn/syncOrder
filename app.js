4f6c66658f10b766b30403d967978ea758afc3ce
@rabalyn <n.mundhenke@mailbox.org>
2018-06-14 22:04:16 +0200

    Add i18next localization to backend

--- app.js
+++ app.js
@@ -10,9 +10,42 @@ import config from './config'
 import hobbitMenu from './lib/hobbitMenu'
 
 import express from 'express'
-import exphbs from 'express-handlebars'
+import session from 'express-session'
+const i18next = require('i18next')
+const i18nextExpressMiddleware = require('i18next-express-middleware')
+const Backend = require('i18next-node-fs-backend')
+const exphbs = require('express-handlebars')
+
+i18next
+  .use(Backend)
+  .use(i18nextExpressMiddleware.LanguageDetector)
+  .init({
+    backend: {
+      loadPath: __dirname + '/public/locales/{{lng}}/translation.json',
+      addPath: __dirname + '/public/locales/{{lng}}/translation.missing.json'
+    },
+    fallbackLng: 'de',
+    preload: ['en', 'de'],
+    saveMissing: true,
+    detection: {
+      order: ['session', 'cookie', 'querystring'],
+    }
+  }, function(err, t) {
+    if(err) {
+      logerror('i18next init failed: %O', err)
+    } else {
+      logdebug('i18next language loaded... translated... %s', i18next.t('language'))
+    }
+  })
+
 const port = process.env.PORT || 9000
 const app = express()
+app.use(session({
+  secret: config.cookie.secret,
+  resave: config.cookie.resave,
+  saveUninitialized: config.cookie.saveUninitialized
+}))
+app.use(i18nextExpressMiddleware.handle(i18next))
 
 global.tableId = fs.readFileSync(config.serialization.tableIdFilestorepath, 'utf-8') || 1
 global.orders = JSON.parse(fs.readFileSync(config.serialization.ordersFilestorepath, 'utf-8')) || []
@@ -36,25 +69,36 @@ const hbs = exphbs.create({
   partialsDir: [
     'views/partials/'
   ],
-  extname: '.hbs'
+  extname: '.hbs',
+  helpers: {
+    t: (key, options) => getLangT(key, options)
+  }
 })
+
+function getLangT(key, options) {
+  return renderT(key, options)
+}
+
+let renderT = i18next.t
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.render('home')
+app.get('*', (req, res, next) => {
+  renderT = req.t
+  next()
 })
+/**
+ * Path-Routes
+ */
+app.get('/', (req, res) => res.render('home'))
+app.get('/hobbit', (req, res) => res.render('hobbit'))
 
 /**
- * XHR-Functions
+ * XHR-Routes
  */
-app.get('/hobbit', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'imgs', 'hobbit.pdf'))
-})
-
 app.get('/getHobbitMenu', hobbitMenu.getMenu)
 
 http.listen(port, () => {
